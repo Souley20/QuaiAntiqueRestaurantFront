36a954c937d4b1f6cb245d71023690d86bea20f5
Souleymane <150073079+Souley20@users.noreply.github.com>
2024-01-03 17:08:55 +0100

    Ajout système routage

---
+++ Router/allRoutes.js
@@ -0,0 +1,9 @@
+import Route from "./Route";
+
+//Définir ici vos routes
+export const allRoutes = [
+    new Route("/", "Accueil", "/pages/home.html"),        
+]
+
+//Le titre s'affiche comme ceci : Route.title -  websitename
+export const websiteName = "Quai Antique";
